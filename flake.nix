b43d7fa10a17af0c26298f03c7930f415c8a4771
seridescent <seridescent@gmail.com>
2025-12-06 21:02:53 -0500

    flake setup

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "hazel";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    crane.url = "github:ipetkov/crane";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ./rust.nix ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # https://flake.parts/overlays.html
+        _module.args.pkgs = import inputs.nixpkgs
+          {
+            inherit system;
+            overlays = [ inputs.rust-overlay.overlays.default ];
+          };
+
+
+        devShells.default = pkgs.mkShell
+          {
+            inputsFrom = [
+              self'.devShells.rust
+            ];
+          };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
---
+++ rust.nix
@@ -0,0 +1,79 @@
+{ inputs, ... }:
+{
+  perSystem = { config, self', pkgs, lib, ... }:
+    let
+      toolchainForPkgs = p:
+        p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+      craneLib = (inputs.crane.mkLib pkgs).overrideToolchain toolchainForPkgs;
+
+      src = craneLib.cleanCargoSource ./.;
+
+      # Common arguments can be set here to avoid repeating them later
+      commonArgs = {
+        inherit src;
+        strictDeps = true;
+
+        buildInputs =
+          [
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
+            # pkgs.libiconv
+          ];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      hazel = craneLib.buildPackage (
+        commonArgs
+        // {
+          inherit cargoArtifacts;
+          doCheck = false;
+        }
+      );
+    in
+    {
+      checks = {
+        inherit hazel;
+
+        hazel-clippy = craneLib.cargoClippy (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+          }
+        );
+
+        hazel-nextest = craneLib.cargoNextest (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            partitions = 1;
+            partitionType = "count";
+            cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+          }
+        );
+      };
+
+      packages = {
+        default = hazel;
+      };
+
+      apps.default = {
+        type = "app";
+        program = "${hazel}/bin/hazel";
+        meta.description = "";
+      };
+
+      devShells.rust = craneLib.devShell {
+        checks = {
+          inherit (self'.checks) hazel hazel-clippy;
+        };
+
+        shellHook = ''
+          # For rust-analyzer 'hover' tooltips to work.
+          export RUST_SRC_PATH="${toolchainForPkgs pkgs}/lib/rustlib/src/rust/library";
+        '';
+      };
+    };
+}
